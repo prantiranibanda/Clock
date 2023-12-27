e122dbc5067ab475490f469db71201f11dbc66d0
pranti <prantibanda@gmail.com>
2023-12-27 19:31:07 +0530

    Created the digital clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
+  useEffect(()=>{
+    setInterval(()=>{
+      setSecond(new Date().getSeconds());
+      setHour((new Date().getHours()>12) ? new Date().getHours()-12 : new Date().getHours());
+      setMinute(new Date().getMinutes());
+    },1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text">
+    {`${hour}:${minute}:${second}`}
     </div>
   );
 }
-
 export default App;
